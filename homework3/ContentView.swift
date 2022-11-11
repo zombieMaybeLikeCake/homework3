4a3c49c5043c1b70f9540b765b1626c65d46c16f
zombieMaybeLikeCake <possibilum@gmail.com>
2022-11-11 12:31:45 +0800

    Initial Commit

---
+++ homework3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework3
+//
+//  Created by robert on 2022/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework3/homework3App.swift
@@ -0,0 +1,17 @@
+//
+//  homework3App.swift
+//  homework3
+//
+//  Created by robert on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct homework3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
